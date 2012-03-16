6266e555935ffea1b73af6ee20153f7109bcc439
Adam Saleh <asaleh@redhat.com>
2012-03-16 11:27:24 +0100

    Pridana podpora pre update v TestPlan

--- src/com/redhat/nitrate/TestPlan.java
+++ src/com/redhat/nitrate/TestPlan.java
@@ -10,14 +10,17 @@ package com.redhat.nitrate;
  * @author asaleh
  */
 public class TestPlan {
-    public static class add_tag extends TcmsCommand{
+    public static class add_tag extends TcmsArrayCommand{
         @RequiredField
         public Integer[] plan_ids;
         @RequiredField
         public String[]  tags;
     }
-
-    public static class create extends TcmsCommand{
+    public static class check_plan_type extends TcmsArrayCommand{
+        @RequiredField
+        public String name;
+    }
+    public static class create extends TcmsHashCommand{
         @RequiredField
         public Integer product;
         @RequiredField
@@ -30,5 +33,35 @@ public class TestPlan {
         public String  text;
         public Integer parent;
         public Boolean is_active;
+    }
+    /*public static class filter extends TcmsCommand{
+        TOO COMPLICATED
+    }*/
+    public static class get extends TcmsArrayCommand{
+        @RequiredField
+        public Integer id;
+    };
+    public static class get_change_history extends TestPlan.get{}
+    public static class get_env_groups extends TestPlan.get{}
+    public static class get_plan_type extends TestPlan.get{}
+    public static class get_product extends TestPlan.get{}
+    public static class get_tags extends TestPlan.get{}
+    public static class get_test_cases extends TestPlan.get{}
+    public static class get_test_runs extends TestPlan.get{}
+    public static class get_text extends TestPlan.get{}
+
+
+    public static class update extends TcmsArrayCommand{
+         public Integer id;
+         public update.Values values=new Values();
+         public static class Values extends TcmsHashCommand{
+             String name;
+             Integer type;
+             Integer product;
+             Integer default_product_version;
+             Integer parent;
+             Boolean is_active;
+             Integer env_group;
+         }
     }
 }
